7aee42cd1188b6220489d193f4b4822bf4d7899f
molo06002 <molo06002@E-PW0234KQ>
2023-09-29 10:50:39 +0200

    Add project files.

---
+++ Inlämnings uppgift 3.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace inlämningsuppgift3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet?, j eller n: ");
+            string x = Console.ReadLine();
+            Console.WriteLine("Hur gammal är du?: ");
+            string y = Console.ReadLine();
+            int R = Convert.ToInt32(y);
+            if (R <= 22 && x == "j")
+            {
+                Console.WriteLine("Du är kvalificerad för jobbet!");
+            }
+            else
+            {
+                Console.WriteLine("Du är tyvärr inte kvalificerad för jobbet!");
+            }
+        }
+    }
+}
